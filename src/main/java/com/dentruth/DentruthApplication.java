cdbf19c42f3cd10faff6ee7bbce9488d6c0dde5a
kahyun0255 <kkh9946@naver.com>
2026-05-13 21:59:12 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/dentruth/DentruthApplication.java
@@ -0,0 +1,13 @@
+package com.dentruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentruthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentruthApplication.class, args);
+	}
+
+}
