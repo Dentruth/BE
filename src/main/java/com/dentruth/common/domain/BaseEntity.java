77fa8e85e9392ae491f1271c8bf758a3c7f0e9b5
kahyun0255 <kkh9946@naver.com>
2026-05-19 20:28:34 +0900

    fix: BaseEntity LocalDateTime 직렬화 제거

--- src/main/java/com/dentruth/common/domain/BaseEntity.java
+++ src/main/java/com/dentruth/common/domain/BaseEntity.java
@@ -1,9 +1,5 @@
 package com.dentruth.common.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -18,14 +14,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreatedDate
     @Column(updatable = false)
     private Instant createdAt;
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @LastModifiedDate
     private Instant updatedAt;
 
