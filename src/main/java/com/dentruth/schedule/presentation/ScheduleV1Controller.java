c6d5a1b171e13bdf34bb41b2622f1ab88bec51ab
yujining3827 <j_yujin_swu22@naver.com>
2026-05-29 00:19:22 +0900

    feat: 일정 생성 API 구현

--- src/main/java/com/dentruth/schedule/presentation/ScheduleV1Controller.java
+++ src/main/java/com/dentruth/schedule/presentation/ScheduleV1Controller.java
@@ -1,2 +1,73 @@
-package com.dentruth.schedule.presentation;public class ScheduleV1Controller {
+package com.dentruth.schedule.presentation;
+
+import com.dentruth.common.jwt.CustomUserDetails;
+import com.dentruth.common.response.ApiResponse;
+import com.dentruth.common.response.code.SuccessStatus;
+import com.dentruth.schedule.application.ScheduleService;
+import com.dentruth.schedule.application.dto.request.CreateScheduleRequest;
+import com.dentruth.schedule.application.dto.request.UpdateScheduleRequest;
+import com.dentruth.schedule.application.dto.response.CreateScheduleResponse;
+import com.dentruth.schedule.application.dto.response.ScheduleDetailResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/schedules")
+@RequiredArgsConstructor
+public class ScheduleV1Controller {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ApiResponse<CreateScheduleResponse> createSchedule(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Valid @RequestBody CreateScheduleRequest request
+    ) {
+
+        return ApiResponse.onSuccess(
+                SuccessStatus.OK,
+                scheduleService.createSchedule(
+                        UUID.fromString(userDetails.getUserId()),
+                        request
+                )
+        );
+    }
+
+    @PutMapping("/{scheduleId}")
+    public ApiResponse<ScheduleDetailResponse> updateSchedule(
+            @PathVariable Long scheduleId,
+            @Valid @RequestBody UpdateScheduleRequest request
+    ) {
+
+        return ApiResponse.onSuccess(
+                SuccessStatus.OK,
+                scheduleService.updateSchedule(scheduleId, request)
+        );
+    }
+
+    @DeleteMapping("/{scheduleId}")
+    public ApiResponse<Void> deleteSchedule(
+            @PathVariable Long scheduleId
+    ) {
+
+        scheduleService.deleteSchedule(scheduleId);
+
+        return ApiResponse.onSuccess(SuccessStatus.OK, null);
+    }
+
+    @GetMapping("/{scheduleId}")
+    public ApiResponse<ScheduleDetailResponse> getSchedule(
+            @PathVariable Long scheduleId
+    ) {
+
+        return ApiResponse.onSuccess(
+                SuccessStatus.OK,
+                scheduleService.getSchedule(scheduleId)
+        );
+    }
 }
