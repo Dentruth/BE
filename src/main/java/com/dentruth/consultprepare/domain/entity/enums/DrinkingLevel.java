43b3e1cb912c186616a25ce03958bc323096eba0
yujining3827 <j_yujin_swu22@naver.com>
2026-06-26 23:35:55 +0900

    fix: DrinkingLevel enum 이름 수정

--- src/main/java/com/dentruth/consultprepare/domain/entity/enums/DrinkingLevel.java
+++ src/main/java/com/dentruth/consultprepare/domain/entity/enums/DrinkingLevel.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum DrinkingLevel {
 
-    NON_SMOKER("Non-drinker", "비음주"),
+    NON_DRINK("Non-drinker", "비음주"),
     OCCASIONAL("Occasional Drinker", "가끔 음주"),
     REGULAR("Regular Drinker", "음주"),
     HEAVY("Heavy Drinker", "잦은 음주");
